668b962f6923190cbbc1dadd63ad6653f6aa38ce
abhishekyadav <yadavabhishek2723@gmail.com>
2023-06-30 01:07:14 +0530

    signup completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const { connection } = require("./config/db");
+const connection = require("./config/db");
 const cors = require("cors");
+const usersRoute = require("./route/users.route");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8080;
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-// app.use("/users");
+app.use("/users", usersRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello This is Home Page");
@@ -15,8 +16,8 @@ app.get("/", (req, res) => {
 
 app.listen(port, async () => {
   try {
-    // await connection();
-    // console.log("db connected");
+    await connection();
+    console.log("db connected");
     console.log("listeneing port", port);
   } catch (error) {
     console.log(error.message);
